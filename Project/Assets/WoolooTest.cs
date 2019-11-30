b92c5e2660b55cee18b5db1a7a5ff50c02e91517
Ravbit <me@ravb.it>
2019-11-30 13:07:57 +0100

    Models imported and starting on BT

---
+++ Project/Assets/WoolooTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoolooTest : MonoBehaviour
+{
+    public int Speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(new Vector3(Speed * Time.deltaTime, 0, 0));
+    }
+}
